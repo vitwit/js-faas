adbed7b26b1325f72b013dfbfe441020d45963fc
anilCSE <anil@vitwit.com>
2019-12-17 16:36:45 +0530

    Cleanup

--- sdk/sdk.sh
+++
@@ -1,1 +0,0 @@
-curl http://localhost/api-doc.json -o /home/sys-597/work/openfass/swagger.json && js-sdkgen --json-file /home/sys-597/work/openfass/swagger.json --name FassJS --version 1.0.0 --base-url http://localhost 
